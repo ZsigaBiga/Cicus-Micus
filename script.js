0b4fcf5db8deaeb55b31cd213619e04225d71359
Szász Áron Zsigmond <12c-szasz@IPARI.SULINET.HU>
2022-12-09 08:27:58 +0100

    Baic setup

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const activeMen = (el) => {
+    var parent = document.getElementById('menuOpts').children;
+
+    for (let i = 0; i < parent.length; i++) {
+        console.log(parent[i].classList)
+        if(parent[i].children[0].classList.contains('active')){
+            parent[i].children[0].classList.remove('active');
+        }
+    }
+
+    el.classList.add('active');
+}
