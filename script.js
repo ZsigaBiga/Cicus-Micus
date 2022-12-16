3dfe4e85b3fa9d86436d5ec3e56e326c9482317a
Szász Áron Zsigmond <12c-szasz@IPARI.SULINET.HU>
2022-12-16 08:30:52 +0100

    Kezdetek

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ const activeMen = (el) => {
     var parent = document.getElementById('menuOpts').children;
 
     for (let i = 0; i < parent.length; i++) {
-        console.log(parent[i].classList)
         if(parent[i].children[0].classList.contains('active')){
             parent[i].children[0].classList.remove('active');
         }
     }
 
     el.classList.add('active');
+}
+
+const nextPic = () => {
+
+}
+
+const prevPic = () => {
+    
 }
